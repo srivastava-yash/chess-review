7f3f045788e5b8f8d775c8fe46286a801a0ab3ed
srivastava-yash <yash.184040@gmail.com>
2024-05-25 13:44:58 -0400

    fetching games from chess.com

--- backend/routes/chess.router.js
+++ backend/routes/chess.router.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { Chess } from "chess.js";
 import pgnParser from "pgn-parser";
+import axios from "axios";
 
 const router = Router();
 
@@ -54,4 +55,35 @@ router.post("/parse", async(req, res) => {
         'positions': positions
     })
 
+});
+
+router.post("/games", async (req, res) => {
+    let {username} = req.body;
+    let {year} = req.body;
+    let {month} = req.body;
+
+    if(!username) {
+        return res.status(400).json({'message': "Invalid Request"});
+    }
+
+    if(!year) {
+        year = new Date().getFullYear();
+    }
+
+    if(!month) {
+        currentMonth = new Date().getMonth() + 1;
+        month = currentMonth.toString();
+        month = month.length > 1 ? month : "0"+month;
+    }
+
+    let games;
+    const url = `https://api.chess.com/pub/player/${username}/games/${year}/${month}`;
+    try {
+        let chessResponse = await axios.get(url);
+        games = chessResponse.games;
+    } catch (err) {
+        return res.status(500).json({'message': "Internal Server Error fetching Chess.com games"});
+    }
+
+    return res.status(200).json(games.reverse());
 });
