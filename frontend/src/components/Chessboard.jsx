8cdb3f801d78aaa455c569a92d4df5b90aeee8a7
srivastava-yash <yash.184040@gmail.com>
2024-05-28 22:59:36 -0400

    added the chess pieces animation

--- frontend/src/components/Chessboard.jsx
+++ frontend/src/components/Chessboard.jsx
@@ -5,10 +5,11 @@ export const Chessboard = ({ board }) => {
         {board.map((row, i) => {
             return <div key={i} className="flex">
                 {row.map((square, j) => {
-                    return <div key={j} className={`w-16 h-16 ${(i+j) % 2 === 0 ? 'bg-orange-500' : 'bg-teal-100'}`}>
+                    const squareRepresentation = String.fromCharCode(97 + (j%8)) + "" + (8-i)
+                    return <div key={j} className={`w-16 h-16 ${(i+j) % 2 === 0 ? 'bg-orange-400' : 'bg-slate-200'}`}>
                         <div className="w-full h-full justify-center flex">
                             <div className="text-black h-full justify-center flex flex-col">
-                                {square ? square.type : "" }
+                                {square ? <img className="w-12" src={`/${square?.color === "b" ? square?.type : `${square?.type?.toUpperCase()}w`}.svg`}/> : null}
                             </div>
                         </div>
                     </div>
